559cda91752548727adfe7b5ab33fc84c17ed3b6
Natanel Guterman <natanelguterman@gmail.com>
2021-05-07 14:23:19 +0300

    Added struct Messgaes Response

--- Trivia/ResponseStructsMsg.h
+++ Trivia/ResponseStructsMsg.h
@@ -1,1 +1,17 @@
 #pragma once
+#include <iostream>
+
+typedef struct LoginResponse
+{
+	unsigned int _status;
+} LoginResponse;
+
+typedef struct SignupResponse
+{
+	unsigned int _status;
+} SignupResponse;
+
+typedef struct ErrorResponse
+{
+	std::string _message;
+}ErrorResponse;
