a9bf0a2ee837472c239f77f861fe248519b1d2de
Karina Yagobov <karinya2604@gmail.com>
2021-05-28 21:11:07 +0300

    Added sqlite3 database field and helper method

--- Trivia/SqliteDataBase.cpp
+++ Trivia/SqliteDataBase.cpp
@@ -1,4 +1,5 @@
 #include "SqliteDataBase.h"
+#include "sqlite3.h"
 
 bool SqliteDataBase::doesUserExist(std::string username)
 {
@@ -12,4 +13,15 @@ bool SqliteDataBase::doesPasswordMatch(std::string password, std::string repeat)
 
 void SqliteDataBase::addNewUser(std::string username, std::string password, std::string mail)
 {
+}
+
+// Helper mehtod.
+void SqliteDataBase::sqlQuery(const char* query, std::string error, int(*ptr)(void*, int, char**, char**))
+{
+    char* errMessage = nullptr;
+    int res = sqlite3_exec(this->_Database, query, ptr, nullptr, &errMessage);
+    if (res != SQLITE_OK)
+    {
+        std::cerr << error << std::endl << errMessage << std::endl << std::endl;
+    }
 }
--- Trivia/SqliteDataBase.h
+++ Trivia/SqliteDataBase.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "IDataBase.h"
+#include <iostream>
 
 class SqliteDataBase : IDataBase
 {
@@ -7,5 +8,9 @@ public:
 	virtual bool doesUserExist(std::string username);
 	virtual bool doesPasswordMatch(std::string password, std::string repeat);
 	virtual void addNewUser(std::string username, std::string password, std::string mail);
+
+private:
+	sqlite3* _Database;
+	void sqlQuery(const char* query, std::string error, int (*ptr)(void*, int, char**, char**)); // Helper Method
 
 };
