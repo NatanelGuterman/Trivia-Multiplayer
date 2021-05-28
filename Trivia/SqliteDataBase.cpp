14927276020acbe62840d25764ade5cf0c7cace6
Karina Yagobov <karinya2604@gmail.com>
2021-05-28 21:18:19 +0300

    Finished doesUserExists()

--- Trivia/SqliteDataBase.cpp
+++ Trivia/SqliteDataBase.cpp
@@ -1,9 +1,21 @@
 #include "SqliteDataBase.h"
 #include "sqlite3.h"
 
+bool exists = false;
+int callback_userExists(void* data, int argc, char** argv, char** azColName);
+
 bool SqliteDataBase::doesUserExist(std::string username)
 {
-    return false;
+    std::string query = "", error = "";
+    sqlQuery(SQL_BEGIN, SQL_BEGIN_ERROR, nullptr);
+
+    query = "SELECT * FROM USERS WHERE Username=" + username + ";";
+    error = "ERROR: doesUserExist() - Couldn't select from USERS.";
+
+    sqlQuery(query.c_str(), error, callback_userExists);
+    sqlQuery(SQL_COMMIT, SQL_COMMIT_ERROR, nullptr);
+
+    return exists;
 }
 
 bool SqliteDataBase::doesPasswordMatch(std::string password, std::string repeat)
@@ -24,4 +36,10 @@ void SqliteDataBase::sqlQuery(const char* query, std::string error, int(*ptr)(vo
     {
         std::cerr << error << std::endl << errMessage << std::endl << std::endl;
     }
+}
+
+int callback_userExists(void* data, int argc, char** argv, char** azColName)
+{
+    exists = (argc == 0 ? false : true);
+    return 0;
 }
--- Trivia/SqliteDataBase.h
+++ Trivia/SqliteDataBase.h
@@ -2,6 +2,12 @@
 #include "IDataBase.h"
 #include <iostream>
 
+#define SQL_BEGIN "BEGIN;"
+#define SQL_COMMIT "COMMIT;"
+#define SQL_BEGIN_ERROR "Couldn't send BEGIN."
+#define SQL_COMMIT_ERROR "Couldn't send COMMIT."
+
+
 class SqliteDataBase : IDataBase
 {
 public:
