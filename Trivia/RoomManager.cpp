b2416de8340cfd991146d899a0fe0433d0b91dae
Karina Yagobov <karinya2604@gmail.com>
2021-06-18 16:25:32 +0300

    Finished deleteRoom()

--- Trivia/RoomManager.cpp
+++ Trivia/RoomManager.cpp
@@ -16,5 +16,20 @@ void RoomManager::createRoom(LoggedUser loggedUser, RoomData roomData)
 	room.set_m_metdata(roomData);
 	room.set_m_users(users);
 
-	this->m_rooms[this->m_rooms.size() + 1] = room;
+	this->m_rooms[roomData.id] = room;
+}
+
+/*
+This function deletes a room fro the map, if the room is in the map.
+input: int ID --> Room's id.
+output: None.
+*/
+
+void RoomManager::deleteRoom(int ID)
+{
+	std::map<int, Room>::iterator iter = this->m_rooms.find(ID);
+	if (iter != this->m_rooms.end())
+	{
+		this->m_rooms.erase(iter);
+	}
 }
