fe1a0851732d4f16051972d5b319e058e2965dd6
Natanel Guterman <natanelguterman@gmail.com>
2021-06-19 20:09:55 +0300

    Added 2.0.0 structs response

--- Trivia/ResponseStructsMsg.h
+++ Trivia/ResponseStructsMsg.h
@@ -2,6 +2,7 @@
 #include <iostream>
 #include "json.hpp"
 #include "EnumCodes.h"
+#include "Room.h"
 
 typedef struct LoginResponse
 {
@@ -26,6 +27,49 @@ typedef struct ErrorResponse
 	NLOHMANN_DEFINE_TYPE_INTRUSIVE(ErrorResponse, _message)
 
 }ErrorResponse;
+
+typedef struct LogoutResponse
+{
+
+	unsigned int _status;
+	NLOHMANN_DEFINE_TYPE_INTRUSIVE(LogoutResponse, _status)
+
+}LogoutResponse;
+
+typedef struct GetRoomsResponse
+{
+
+	unsigned int _status;
+	NLOHMANN_DEFINE_TYPE_INTRUSIVE(GetRoomsResponse, _status)
+	std::vector<RoomData> _rooms; //!
+
+}GetRoomsResponse;
+
+typedef struct GetPlayersInRoomResponse
+{
+	std::vector<std::string> _players;
+	NLOHMANN_DEFINE_TYPE_INTRUSIVE(GetPlayersInRoomResponse, _players)
+
+}GetPlayersInRoomResponse;
+
+typedef struct JoinRoomResponse
+{
+
+	unsigned int _status;
+	NLOHMANN_DEFINE_TYPE_INTRUSIVE(JoinRoomResponse, _status)
+
+}JoinRoomResponse;
+
+typedef struct CreateRoomResponse
+{
+
+	unsigned int _status;
+	NLOHMANN_DEFINE_TYPE_INTRUSIVE(CreateRoomResponse, _status)
+
+}CreateRoomResponse;
+
+
+
 
 
 
