96c10fac32bfeda3365a09c91d21ce1e449adc03
Natanel Guterman <natanelguterman@gmail.com>
2021-07-03 20:19:30 +0300

    Added Menu design

--- TriviaClient/Menu.xaml.cs
+++ TriviaClient/Menu.xaml.cs
@@ -19,9 +19,23 @@ namespace TriviaClient
 {
     public sealed partial class Menu : Page
     {
+        DispatcherTimer _dispatcherTimer;
         public Menu()
         {
             this.InitializeComponent();
+            this.InitializeComponent();
+            _dispatcherTimer = new DispatcherTimer();
+            _dispatcherTimer.Tick += dispatcherTimer_Tick;
+            _dispatcherTimer.Interval = TimeSpan.FromSeconds(1);
+            _dispatcherTimer.Start();
+        }
+
+        private void dispatcherTimer_Tick(object sender, object e)
+        {
+            if (input.Visibility == Visibility.Visible)
+                input.Visibility = Visibility.Collapsed;
+            else
+                input.Visibility = Visibility.Visible;
         }
 
         private void createRoomButton_Click(object sender, RoutedEventArgs e)
@@ -32,6 +46,16 @@ namespace TriviaClient
         private void joinRoomButton_Click(object sender, RoutedEventArgs e)
         {
             Frame.Navigate(typeof(JoinRoom));
+        }
+
+        private void input_GotFocus(object sender, RoutedEventArgs e)
+        {
+            _dispatcherTimer.Stop();
+        }
+
+        private void input_LostFocus(object sender, RoutedEventArgs e)
+        {
+            _dispatcherTimer.Start();
         }
     }
 }
